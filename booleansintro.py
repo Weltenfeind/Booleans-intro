0ed96eb8a375dfa660bd8c66adae9b24caaba9c1
Weltenfeind <121118273+Weltenfeind@users.noreply.github.com>
2022-12-27 18:31:34 +0100

    Add files via upload

---
+++ booleansintro.py
@@ -0,0 +1,14 @@
+is_single = True
+print(type(is_single))
+
+#para modificar valor
+is_single = False 
+print(is_single)
+
+#existe un operador llamado "not" que precisamente invierte el valor:
+
+print(not True)
+print(not False)
+
+is_single = not is_single 
+print(is_single) 
